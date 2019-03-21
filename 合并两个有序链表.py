18e7a262929eca940e3ab6e9cfdf25d49c4a1f57
李瑞琴 <2482003411@qq.com>
2019-03-21 16:23:56 +0800

    合并两个有序表

--- 合并两个有序链表.py
+++ 合并两个有序链表.py
@@ -6,16 +6,89 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
         p = l1
+        q = None
         while p != None:
             q = p
             p = p.next
         q.next = l2
-        m = l1
-        while m != None:
-            while m.next != None:
-                if m.val > m.next.val:
-                    m.val, m.next.val = m.next.val, m.val
-                m = m.next
-
+        l1 = self.sortListNode(l1)
         return l1
+
+    def sortListNode(self, l1: ListNode) -> ListNode:
+
+        # write your code here
+        if (l1 == None): return l1
+
+        def findmid(head):
+            count = 0
+            save = head
+            while (head != None):
+                head = head.next
+                count += 1
+            i = 0
+            while (True):
+
+                i += 1
+                if (i >= count / 2): break
+                save = save.next
+            return save
+
+        def merge(left, right):
+            pre = ListNode(None)
+            save = pre
+            while (left != None and right != None):
+                if (left.val < right.val):
+                    pre.next = left
+                    pre = left
+                    left = left.next
+                else:
+                    pre.next = right
+                    pre = right
+                    right = right.next
+            if (left == None):
+                while (right != None):
+                    pre.next = right
+                    pre = right
+                    right = right.next
+
+            if (right == None):
+                while (left != None):
+                    pre.next = left
+                    pre = left
+                    left = left.next
+
+            return save.next
+
+        def mergeSort(head):
+            if (head.next == None): return head
+            mid = findmid(head)
+            midn = mid.next
+            mid.next = None
+            left = mergeSort(head)
+            right = mergeSort(midn)
+
+            result = merge(left, right)
+            return result
+
+        result = mergeSort(l1)
+        return result
+
+
+if __name__ == '__main__':
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    so = Solution()
+    l3 = so.mergeTwoLists(l1,l2)
+    p = l3
+    while p!=None:
+        print(p.val)
+        p = p.next
